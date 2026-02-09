711ee261c94156f18510f588411ca61357395e14
George K <gkwambata@gmail.com>
2026-02-09 12:49:06 +0300

    fix: partial implementation of shortcut service with database check

--- index.php
+++ index.php
@@ -1,24 +1,41 @@
 <?php
 require __DIR__ . '/vendor/autoload.php';
-
-//Create database connection
 use Illuminate\Database\Capsule\Manager as Capsule;
 
+
+#check https://github.com/vlucas/phpdotenv
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
+$dotenv->safeLoad();
+
+
+//Create database connection
+# check https://packagist.org/packages/illuminate/database
 $capsule = new Capsule;
 
 $capsule->addConnection([
-    'driver' => 'mysql',
-    'host' => 'localhost',
-    'database' => 'database',
-    'username' => 'root',
-    'password' => 'password',
+    'driver' => $_ENV['DB_CONNECTION'],
+    'host' =>  $_ENV['DB_HOST'],
+    'database' => $_ENV['DB_DATABASE'],
+    'username' => $_ENV['DB_USERNAME'],
+    'password' => $_ENV['DB_PASSWORD'],
     'charset' => 'utf8',
     'collation' => 'utf8_unicode_ci',
     'prefix' => '',
 ]);
-// Capture url in request
+class ShortUrl extends Illuminate\Database\Eloquent\Model {}
 
+
+// Capture url in request
+$code = $_SERVER['QUERY_STRING'] ?? '#';
 
 // Query database
 
+$shortUrl = ShortUrl::query()->where('shortcode', '=', $code)->get();
 //Redirect to  original url
+$url = $shortUrl[0]->original_url ?? $_ENV['NOTFOUND_URL'];
+
+header("Location: ".$url);
+exit();
+
+
+
