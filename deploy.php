939a15a3c3c5150f7864fdcf91216fe7e704a976
George K <gkwambata@gmail.com>
2026-02-12 12:52:30 +0300

    fix: add deployer as a dependency and update

---
+++ deploy.php
@@ -0,0 +1,42 @@
+﻿<?php
+namespace Deployer;
+
+require 'recipe/composer.php';
+
+// Config
+try {
+
+
+    // Config
+    // Use ssh multiplexing for speedup native ssh client.
+    set('ssh_multiplexing', false);
+
+    set('git_ssh_command', 'ssh');
+
+    set('repository', 'git@github.com:Caricanary/ShortCutService.git');
+
+    // [Optional] Allocate tty for git clone. Default value is false.
+
+    set('git_tty', true);
+
+    set('default_timeout', 720);
+}catch (\Exception $e){
+    echo $e->getMessage();
+}
+add('shared_files', []);
+add('shared_dirs', []);
+add('writable_dirs', []);
+
+// Hosts
+
+host('canary_shortcut')
+    ->set('remote_user', 'web')
+    ->set('port', 22)
+    ->set('deploy_path', '/var/www/html')
+    ->set('forwardAgent', true)
+    ->set('branch', 'main')
+    ->set('labels', ['stage' => 'main']);
+
+// Hooks
+
+after('deploy:failed', 'deploy:unlock');
